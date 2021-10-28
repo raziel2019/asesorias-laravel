05f599210798a402dfcc6ee9098aeffa2d0d871a
RudyUh <rudimyth@hotmail.com>
2021-10-27 21:01:36 -0500

    Login y Registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,32 +6,25 @@
     <div class="container mt--8 pb-5">
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
-                <div class="card bg-secondary shadow border-0">
-                    <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
-                        <div class="btn-wrapper text-center">
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
-                                <span class="btn-inner--text">{{ __('Github') }}</span>
-                            </a>
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
-                                <span class="btn-inner--text">{{ __('Google') }}</span>
-                            </a>
+                <div class="card bg-secondary shadow border-0" >
+                    
+                    <div class="text-center">
+                        <div class="card-profile-image mb-5">
+                            <img  class="rounded-circle " src="{{ asset('imagenes/foto.jpg') }}" style="width:10em; height: 10em;">
                         </div>
                     </div>
-                    <div class="card-body px-lg-5 py-lg-5">
-                        <div class="text-center text-muted mb-4">
-                            <small>
-                                    Create new account OR Sign in with these credentials:
-                                    <br>
-                                    Username <strong>admin@argon.com</strong> Password: <strong>secret</strong>
-                            </small>
+
+                    <div class="card-body px-lg-5 py-lg-5 mt-4">
+                        <div class="text-center text-muted mb-4 mt-1">
+                            <small style="font-size:17px"> <strong>¡Hola bienvenido!</strong></small>
+                            <br>
+                            <small style="font-size:13px">Inicia sesión para comenzar</small>
                         </div>
                         <form role="form" method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-3">
+                                <small style="font-size: 12px">Correo electrónico</small>
                                 <div class="input-group input-group-alternative">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="ni ni-email-83"></i></span>
@@ -45,6 +38,7 @@
                                 @endif
                             </div>
                             <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                <small style="font-size: 12px">Contraseña</small>
                                 <div class="input-group input-group-alternative">
                                     <div class="input-group-prepend">
                                         <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
@@ -64,7 +58,7 @@
                                 </label>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
+                                <button type="submit" class="btn btn-primary btn-block my-4" style="font-size:16px">{{ __('Acceder') }}</button>
                             </div>
                         </form>
                     </div>
