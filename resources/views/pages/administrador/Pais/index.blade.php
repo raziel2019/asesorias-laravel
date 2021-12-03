c7d884e9a1efb5470075ffd0b28d06bc189c9c73
Raziel Hernandez <hraziel487@gmail.com>
2021-12-02 21:55:10 -0600

    geolocalizacion

--- resources/views/pages/administrador/Pais/index.blade.php
+++ resources/views/pages/administrador/Pais/index.blade.php
@@ -29,6 +29,10 @@
                                 <th scope="col">Nombre</th>
                                 <th scope="col">Fecha de Creación</th>
                                 <th scope="col">Acciones</th>
+                                <th scope="col">
+                                    <p><button onclick="geoFindMe()">Show my location</button></p>
+                                    <div id="out"></div>
+                                </th>
                             </tr>
                         </thead>
                         @foreach ($paises as $orden=>$pais)
@@ -62,6 +66,35 @@
     </div>
     
 </div>
+<script>
+function geoFindMe() {
+    var output = document.getElementById("out");
+  
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
+  
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+  
+      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+  
+      var img = new Image();
+      img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+  
+    };
+  
+    function error() {
+      output.innerHTML = "Unable to retrieve your location";
+    };
+  
+    output.innerHTML = "<p>Locating…</p>";
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+</script>
 @endsection
 
 
